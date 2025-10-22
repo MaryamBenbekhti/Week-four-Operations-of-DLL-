afbd34ad4d9144ee42bf64b9de52e52cb0599f5e
MaryamBenbekhti <marie.benbekhti@gmail.com>
2025-10-23 01:05:39 +0300

    Add files via upload

---
+++ Adding element at the end of DLL.c
@@ -0,0 +1,20 @@
+struct Node* insertEnd(struct Node* head, int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (head == NULL) {
+        newNode->prev = NULL;
+        return newNode;
+    }
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    newNode->prev = temp;
+
+    return head;
+}
+
